d1ff02678d9f6c4fc14b8722074efdc282bd61cd
Zack <zixux@hotmail.com>
2017-03-05 17:08:09 +0000

    Improved autocorrelation script

--- autocorrelation.py
+++ autocorrelation.py
@@ -1,35 +1,39 @@
+"""
+Zack Xiang & Isaac Scott
+Auto-correlation and S calculation
+"""
+
 import numpy as np
-import csv
 import os
 import matplotlib.pyplot as plt
+
+
+def stop_if_below_zero(sequence):
+    for i, val in enumerate(sequence):
+        if val <= 0:
+            return sequence[:i]
+    return sequence
 
 
 def auto_correlation(data):
     n = len(data)
     x = np.array(data)
     x = x - x.mean()
-    # Correlate using some convolution
     return np.correlate(x, x, mode='full')[-n:] / (x.var() * (np.arange(n, 0, -1)))
 
 
-data_dir = 'pond'
-taus = np.arange(0, 600, 0.1)
+# The directory contains all the measurement data that require auto-correlation analysis
+data_dir = 'grass'
+labels = ('Adam', 'Brian', 'Camilla')
 
 for subdir, dirs, files in os.walk(data_dir):
     for file in files:
         data = np.loadtxt(os.path.join(subdir, file))
-        Rs = auto_correlation(data)
-        plt.plot(Rs[:100])
-        print(sum(Rs[:100]))
+        Rs = stop_if_below_zero(auto_correlation(data))
+        plt.plot(Rs, label=file)
+        print(sum(Rs))
 
-# randoms = np.random.uniform(0., 1., size=10000)
-# plt.plot(auto_correlation(randoms))
-# print(sum(auto_correlation(randoms)[:-1000]))
-
-# sins = np.sin(taus)
-# sin_rs = auto_correlation(sins)
-# plt.plot(taus, sin_rs)
-# print(sum(sin_rs))
-
-
+plt.legend(loc=0)
+plt.xlabel(r'$\tau (s)$')
+plt.ylabel(r'$R(\tau)$')
 plt.show()
